5a6cd266ff19a5e2d29642f77fe9e2734f2013b1
Shashiwadana Nirmani <shashiwadana@ascentic.se>
2023-03-03 09:32:11 +0530

    padding updated

--- bs_video_player/lib/src/playback_speed_menu.dart
+++ bs_video_player/lib/src/playback_speed_menu.dart
@@ -20,7 +20,7 @@ class PlaybackSpeedMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(15),
+      padding: const EdgeInsets.all(20),
       child: Align(
         alignment: Alignment.topLeft,
         child: PopupMenuButton<double>(
